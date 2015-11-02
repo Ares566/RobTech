540e2e3d1e7c13204bd2554738a322081f55e0ca
Renat Abaidulin <ares566@ya.ru>
2015-11-02 18:59:46 +0300

    Эксперимент №1 «Перемещение на скорость»

--- Examples/Ex1/Ex1.ino
+++ Examples/Ex1/Ex1.ino
@@ -15,16 +15,15 @@ boolean finish = false;
 void setup() {
   Robot.initAR(AR_M_WITH_ENC && AR_M_WITH_LCD);
   Robot.printString("RobTech", 5, 0); // Печатаем на LCD строку
-
 }
+
+
 
 // здесь мы пишем основной код логики робота, 
 // функция loop вызывается заново как только ее работа была закончена
 void loop() {
-	if(!finish){// выполняем пока не финишировали
-				
-		Robot.moveForward(AR_MAX_SPEED,30); // проехать на максимальной скорости 30 см
-		
-		finish = true; // поднять финишный флаг
-	}
+    if(!finish){// выполняем пока не финишировали		
+	  Robot.moveForward(AR_MAX_SPEED, 30); // проехать на максимальной скорости 30 см		
+	  finish = true; // поднять финишный флаг
+    }
 }
